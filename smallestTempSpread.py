ebd53d33d206b5c6b0ec3d36d0ca3c779d97b77d
Kenneth Chuen <kc3334@columbia.edu>
2020-12-22 02:06:02 -0500

    Finished problem 1)

---
+++ smallestTempSpread.py
@@ -0,0 +1,18 @@
+import re 
+
+minDiff = float('inf')
+
+with open('w_data.dat') as file:
+    for line in file.readlines()[6:36]: # read only lines 7-36
+        cols = line.rstrip().split()
+        currMaxTemp = ''.join(re.findall(r'[\d.]+', cols[1])) # use regex to filter out * char
+        currMinTemp = ''.join(re.findall(r'[\d.]+', cols[2]))
+        currDiff = float(currMaxTemp) - float(currMinTemp)
+
+        if (currDiff < minDiff):
+            minDiff = currDiff
+            minDayNum = cols[0]
+        
+        # print(cols[0], currMaxTemp, currMinTemp, currDiff)
+
+print(minDayNum)
